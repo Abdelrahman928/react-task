01cd763472fae11889ecd01c5016d800608d0caf
Abdelrahman <abdelrahman.ahmad221@gmail.com>
2024-10-17 02:24:13 +0300

    modified:   src/pages/AddProduct.jsx 	modified:   src/pages/HomePage.jsx

--- src/pages/AddProduct.jsx
+++ src/pages/AddProduct.jsx
@@ -87,7 +87,7 @@ function AddProduct(){
             ...specificAttributes
         };
 
-        axios.post('http://abdelrahman-task.42web.io/add-product', productAttributes)
+        axios.post('https://abdelrahman-task.42web.io/add-product', productAttributes)
         .then(response => {
             console.log('Response:', response.data);
             navigate('/');
